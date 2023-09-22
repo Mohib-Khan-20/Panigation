c6df04731ac6e5c8796cbfb0b280cead4b2c0c16
Mohib-Khan-20 <mohibkhan234579@gmail.com>
2023-09-22 02:15:05 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,161 @@
+function cli1(){
+    let pani1= document.getElementById("pani1");
+    pani1.style.cssText += " background-color: rgba(185, 185, 185, 0.493);"
+    pani2.style.cssText += "background-color:white;"
+    pani3.style.cssText += "background-color:white;"
+    pani4.style.cssText += "background-color:white;"
+    pani5.style.cssText += "background-color:white;"
+    pani6.style.cssText += "background-color:white;"
+    pani7.style.cssText += "background-color:white;"
+    pani8.style.cssText += "background-color:white;"
+    pani9.style.cssText += "background-color:white;"
+    pani10.style.cssText += "background-color:white;"
+}
+function cli2(){
+    let pani2= document.getElementById("pani2");
+    pani2.style.cssText += " background-color: rgba(185, 185, 185, 0.493);"
+    pani1.style.cssText += "background-color:white;"
+    pani3.style.cssText += "background-color:white;"
+    pani4.style.cssText += "background-color:white;"
+    pani5.style.cssText += "background-color:white;"
+    pani6.style.cssText += "background-color:white;"
+    pani7.style.cssText += "background-color:white;"
+    pani8.style.cssText += "background-color:white;"
+    pani9.style.cssText += "background-color:white;"
+    pani10.style.cssText += "background-color:white;"
+}
+function cli3(){
+    let pani3= document.getElementById("pani3");
+    pani3.style.cssText += " background-color: rgba(185, 185, 185, 0.493);"
+    pani1.style.cssText += "background-color:white;"
+    pani2.style.cssText += "background-color:white;"
+    pani4.style.cssText += "background-color:white;"
+    pani5.style.cssText += "background-color:white;"
+    pani6.style.cssText += "background-color:white;"
+    pani7.style.cssText += "background-color:white;"
+    pani8.style.cssText += "background-color:white;"
+    pani9.style.cssText += "background-color:white;"
+    pani10.style.cssText += "background-color:white;"
+}
+function cli4(){
+    let pani4= document.getElementById("pani4");
+    pani4.style.cssText += " background-color: rgba(185, 185, 185, 0.493);"
+    pani1.style.cssText += "background-color:white;"
+    pani2.style.cssText += "background-color:white;"
+    pani5.style.cssText += "background-color:white;"
+    pani3.style.cssText += "background-color:white;"
+    pani6.style.cssText += "background-color:white;"
+    pani7.style.cssText += "background-color:white;"
+    pani8.style.cssText += "background-color:white;"
+    pani9.style.cssText += "background-color:white;"
+    pani10.style.cssText += "background-color:white;"
+}
+function cli5(){
+    let pani5= document.getElementById("pani5");
+    pani5.style.cssText += " background-color: rgba(185, 185, 185, 0.493);"
+    pani1.style.cssText += "background-color:white;"
+    pani2.style.cssText += "background-color:white;"
+    pani3.style.cssText += "background-color:white;"
+    pani4.style.cssText += "background-color:white;"
+    pani6.style.cssText += "background-color:white;"
+    pani7.style.cssText += "background-color:white;"
+    pani8.style.cssText += "background-color:white;"
+    pani9.style.cssText += "background-color:white;"
+    pani10.style.cssText += "background-color:white;"
+}
+function cli6(){
+    let pani6= document.getElementById("pani6");
+    pani6.style.cssText += " background-color: rgba(185, 185, 185, 0.493);"
+    pani1.style.cssText += "background-color:white;"
+    pani2.style.cssText += "background-color:white;"
+    pani3.style.cssText += "background-color:white;"
+    pani4.style.cssText += "background-color:white;"
+    pani5.style.cssText += "background-color:white;"
+    pani7.style.cssText += "background-color:white;"
+    pani8.style.cssText += "background-color:white;"
+    pani9.style.cssText += "background-color:white;"
+    pani10.style.cssText += "background-color:white;"
+}
+function cli7(){
+    let pani7= document.getElementById("pani7");
+    pani7.style.cssText += " background-color: rgba(185, 185, 185, 0.493);"
+    pani1.style.cssText += "background-color:white;"
+    pani2.style.cssText += "background-color:white;"
+    pani3.style.cssText += "background-color:white;"
+    pani4.style.cssText += "background-color:white;"
+    pani5.style.cssText += "background-color:white;"
+    pani6.style.cssText += "background-color:white;"
+    pani8.style.cssText += "background-color:white;"
+    pani9.style.cssText += "background-color:white;"
+    pani10.style.cssText += "background-color:white;"
+}
+function cli8(){
+    let pani8= document.getElementById("pani8");
+    pani8.style.cssText += " background-color: rgba(185, 185, 185, 0.493);"
+    pani1.style.cssText += "background-color:white;"
+    pani2.style.cssText += "background-color:white;"
+    pani3.style.cssText += "background-color:white;"
+    pani4.style.cssText += "background-color:white;"
+    pani5.style.cssText += "background-color:white;"
+    pani6.style.cssText += "background-color:white;"
+    pani7.style.cssText += "background-color:white;"
+    pani9.style.cssText += "background-color:white;"
+    pani10.style.cssText += "background-color:white;"
+}
+function cli9(){
+    let pani9= document.getElementById("pani9");
+    pani9.style.cssText += " background-color: rgba(185, 185, 185, 0.493);"
+    pani1.style.cssText += "background-color:white;"
+    pani2.style.cssText += "background-color:white;"
+    pani3.style.cssText += "background-color:white;"
+    pani4.style.cssText += "background-color:white;"
+    pani5.style.cssText += "background-color:white;"
+    pani6.style.cssText += "background-color:white;"
+    pani7.style.cssText += "background-color:white;"
+    pani8.style.cssText += "background-color:white;"
+    pani10.style.cssText += "background-color:white;"
+}
+function cli10(){
+    let pani10= document.getElementById("pani10");
+    pani10.style.cssText += " background-color: rgba(185, 185, 185, 0.493);"
+    pani1.style.cssText += "background-color:white;"
+    pani2.style.cssText += "background-color:white;"
+    pani3.style.cssText += "background-color:white;"
+    pani4.style.cssText += "background-color:white;"
+    pani5.style.cssText += "background-color:white;"
+    pani6.style.cssText += "background-color:white;"
+    pani7.style.cssText += "background-color:white;"
+    pani8.style.cssText += "background-color:white;"
+    pani9.style.cssText += "background-color:white;"
+}
+
+flag=0
+
+function next1(x){
+    flag= flag+ x
+    slide(flag)
+}
+
+function slide(num){
+let b = document.getElementsByClassName("panigation-2");
+
+if(num==b.length){
+    flag=0
+    num=0
+}
+if(num<0){
+    flag=b.length-1
+    num=b.length-1
+    
+}
+
+for (let y of b){
+    y.style.cssText +=" background-color: white;"
+}
+// b[num].style.cssText += " background-color: rgba(185, 185, 185, 0.493);"
+b[num].style.backgroundColor = "rgba(185, 185, 185, 0.493)";
+
+}
+slide(flag)
+
+
